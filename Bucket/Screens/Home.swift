e2ab1a88710a50f5290789691ffb768b4b0f11df
Bezaleel Ashefor <3107111+knightbenax@users.noreply.github.com>
2023-12-04 21:11:37 +0100

    First Push

---
+++ Bucket/Screens/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  Bucket
+//
+//  Created by Bezaleel Ashefor on 04/12/2023.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack{
+            
+        }
+    }
+}
+
+#Preview {
+    Home()
+}
---
+++ Bucket/Screens/Views/Standing.swift
@@ -0,0 +1,18 @@
+//
+//  Standing.swift
+//  Bucket
+//
+//  Created by Bezaleel Ashefor on 04/12/2023.
+//
+
+import SwiftUI
+
+struct Standing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Standing()
+}
