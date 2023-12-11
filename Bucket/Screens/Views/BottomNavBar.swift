55090b055c0ee5eb233d2df72560a0585aa4fcb4
Bezaleel Ashefor <knightbenax@gmail.com>
2023-12-11 07:16:36 +0100

    Swipe

--- Bucket/Screens/Views/BottomNavBar.swift
+++ Bucket/Screens/Views/BottomNavBar.swift
@@ -11,6 +11,9 @@ struct BottomNavBar: View {
     var verticalPadding : CGFloat = 9
     var topPadding : CGFloat = 0.5
     @Binding var currentScreen : CURRENT_SCREEN
+    @State var offsetValue : CGFloat = -(400 / 3)
+    var barWidth : CGFloat = 400
+    var pillWidth : CGFloat = 30
     
     var body: some View {
         ZStack(alignment: .top){
@@ -18,24 +21,33 @@ struct BottomNavBar: View {
             HStack(spacing: 0){
                 Button(action: {
                     currentScreen = .STANDINGS
+                    withAnimation{
+                        offsetValue = -(barWidth / 3)
+                    }
                 }){
                     Text("Standings")
                 }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center).padding(.vertical, verticalPadding).padding(.top, topPadding).padding(.horizontal).background(Color.blue)
                 Button(action: {
                     currentScreen = .STATS
+                    withAnimation{
+                        offsetValue = 0
+                    }
                 }){
                     Text("Stats")
                 }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center).padding(.vertical, verticalPadding).padding(.top, topPadding).padding(.horizontal).background(Color.yellow)
                 Button(action: {
                     currentScreen = .PLAYERS
+                    withAnimation{
+                        offsetValue = (barWidth / 3)
+                    }
                 }){
                     Text("Players")
                 }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center).padding(.vertical, verticalPadding).padding(.top, topPadding).padding(.horizontal).background(Color.red)
             }.background(Color.gray.opacity(0.4)).clipShape(RoundedRectangle(cornerRadius: 30)).foregroundStyle(Color.black)
             HStack{
                 
-            }.frame(width: 30, height: 4.5).background(Color.black).clipShape(RoundedRectangle(cornerRadius: 4, style: .continuous))
-        }.padding(.bottom, 10).frame(maxWidth: 400)
+            }.frame(width: pillWidth, height: 4.5).background(Color.black).clipShape(RoundedRectangle(cornerRadius: 4, style: .continuous)).offset(x: offsetValue)
+        }.padding(.bottom, 10).frame(width: barWidth)
     }
 }
 
--- Bucket/Screens/Views/Sections/Standings.swift
+++ Bucket/Screens/Views/Sections/Standings.swift
@@ -10,7 +10,15 @@ import SwiftUI
 struct Standings: View {
     var body: some View {
         VStack{
-            Text("Standings - 32 Players. 10 Remaining").font(.custom(FontsManager.Bold, size: 20))
+            HStack(alignment: .center){
+                Text("Standings - 32 Players. 10 Remaining").font(.custom(FontsManager.Bold, size: 20))
+                Spacer()
+                Button(action: {
+                    
+                }){
+                    Image(systemName: "slider.horizontal.3").font(.system(size: 22, weight: .medium))
+                }
+            }
             Spacer()
         }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading).padding(30)
     }
